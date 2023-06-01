a3ebd2be5b07f15a80a89dc5fe0136db8ec60372
EduBox <123496427+EduardoEduBox@users.noreply.github.com>
2023-06-01 20:45:21 -0300

    media query for header almost done

--- logic.js
+++ logic.js
@@ -44,3 +44,67 @@ function handleResize() {
 }
 
 window.addEventListener("resize", handleResize);
+
+// Header part
+
+const headerBackgroundImages = document.querySelector(
+  "#headerBackgroundImages"
+);
+const headerCharacterImages = document.querySelector("#headerCharacterImages");
+const headerBall = document.querySelector(".headerImageBall");
+
+class Images {
+  constructor(id, backgroundImage, characterImage, ballColor) {
+    this.id = id;
+    this.backgroundImage = backgroundImage;
+    this.characterImage = characterImage;
+    this.ballColor = ballColor;
+  }
+}
+
+const config1 = new Images(
+  1,
+  "Css/assets/backgrounds/backgroundHeader.png",
+  "Css/assets/characters/demonio_cap_0_site_prototipo.png",
+  "rgb(255, 0, 0)"
+);
+
+const config2 = new Images(
+  2,
+  "Css/assets/backgrounds/backgroundHeader2.png",
+  "Css/assets/characters/málanus_cap_2_site_prototipo.png",
+  "#9400d3"
+);
+
+const config3 = new Images(
+  3,
+  "Css/assets/backgrounds/backgroundHeader3.png",
+  "Css/assets/characters/singer_cap_3_site_prototipo.png",
+  "rgb(90, 206, 255)"
+);
+
+const changeHeaderImages = [config1, config2, config3];
+
+// seting the first object as default when you load the page
+
+headerBackgroundImages.style.backgroundImage = `url('${config1.backgroundImage}')`;
+headerCharacterImages.src = config1.characterImage;
+headerBall.style.backgroundColor = config1.ballColor;
+
+let i = 1;
+
+setInterval(() => {
+  if (i < changeHeaderImages.length) {
+    headerBackgroundImages.style.backgroundImage = `url('${changeHeaderImages[i].backgroundImage}')`;
+    headerCharacterImages.src = changeHeaderImages[i].characterImage;
+    headerBall.style.backgroundColor = changeHeaderImages[i].ballColor;
+
+    i++;
+  } else {
+    headerBackgroundImages.style.backgroundImage = `url('${changeHeaderImages[0].backgroundImage}')`;
+    headerCharacterImages.src = changeHeaderImages[0].characterImage;
+    headerBall.style.backgroundColor = changeHeaderImages[0].ballColor;
+
+    i = 1;
+  }
+}, 10000);
