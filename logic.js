d9a67fcdbb080f3d0b56cb14badb6acf90380285
EduBox <123496427+EduardoEduBox@users.noreply.github.com>
2023-05-31 22:29:42 -0300

    navBar with media query done

--- logic.js
+++ logic.js
@@ -1,7 +1,7 @@
 // Nav bar part
 
 const hamburguerIcon = document.querySelector("#menuIcon");
-const navList = document.querySelector(".navList");
+const navElements = document.createElement("div");
 
 hamburguerIcon.addEventListener("click", function () {
   hamburguerIcon.classList.toggle("hamburguerToggled");
@@ -9,10 +9,38 @@ hamburguerIcon.addEventListener("click", function () {
   if (hamburguerIcon.classList.contains("hamburguerToggled")) {
     hamburguerIcon.src = "Css/assets/logoChanged.png";
 
-    // here is the code to make the list appear when you click
+    // Code to make the list appear when you click
+    navElements.classList.add("navMediaQuery");
+    navElements.innerHTML = `
+      <ul class="navList2">
+        <li><a href="#header">Home</a></li>
+        <li><a href="#chapters">Capítulos</a></li>
+        <li><a href="">Personagens</a></li>
+        <li><a href="">Sobre mim</a></li>
+        <li><hr></li> 
+      </ul>`;
 
-    //......
+    document.body.appendChild(navElements);
+
+    setTimeout(() => {
+      navElements.style.right = "0";
+    }, 0);
+
+    console.log("if");
   } else {
     hamburguerIcon.src = "Css/assets/icon.png";
+
+    navElements.style.right = `calc(-${navElements.offsetWidth}px - 1.5rem)`;
+
+    console.log("else");
   }
 });
+
+function handleResize() {
+  if (window.innerWidth > 1279) {
+    // Remove navElements from the document.body
+    navElements.remove();
+  }
+}
+
+window.addEventListener("resize", handleResize);
