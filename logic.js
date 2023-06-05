dc0c8420f5585e3b53fe3a9b7944c9d44edec150
EduBox <123496427+EduardoEduBox@users.noreply.github.com>
2023-06-05 20:06:33 -0300

    tooltip for chapter section created (need improvement)

--- logic.js
+++ logic.js
@@ -142,43 +142,116 @@ const images = document.getElementsByClassName("prePages");
 const textElement = document.getElementById("chaptersTitle");
 const backgroundTextElement = document.querySelector(".backgroundText");
 
+let currentTooltip = null; // Add this line to declare the currentTooltip variable
+
+let description = ""; // this variable is the description of those tooltips for each chapter
+let srcImages = ""; // source for the tooltip images
+
 for (let i = 0; i < images.length; i++) {
   images[i].addEventListener("mouseover", function () {
     switch (i) {
       case 0:
         textElement.textContent = "Cap - 0: Invasão";
         backgroundTextElement.textContent = "Cap - 0: Invasão";
+
+        description =
+          "Uma grande invasão na vila Yasáshi, humanos contra pessoas que se entitulam 'demônios', quem saiu vitorioso?";
+        srcImages = "Css/assets/tooltipPages/random1.png";
         break;
       case 1:
         textElement.textContent = "Cap - 1: Padaria";
         backgroundTextElement.textContent = "Cap - 1: Padaria";
+
+        description = "Description for Cap - 1";
+        srcImages = "Css/assets/tooltipPages/random2.png";
         break;
       case 2:
         textElement.textContent = "Cap - 2: Pai e irmão";
         backgroundTextElement.textContent = "Cap - 2: Pai e irmão";
+
+        description = "Description for Cap - 2";
+        srcImages = "Css/assets/tooltipPages/random3.png";
         break;
       case 3:
         textElement.textContent = "Cap - 3: Paisagem";
         backgroundTextElement.textContent = "Cap - 3: Paisagem";
+
+        description = "Description for Cap - 2";
+        srcImages = "Css/assets/tooltipPages/random4.png";
         break;
       case 4:
         textElement.textContent = "Cap - 4: Escola";
         backgroundTextElement.textContent = "Cap - 4: Escola";
+
+        description = "Description for Cap - 2";
+        srcImages = "Css/assets/tooltipPages/random5.png";
         break;
       default:
         textElement.textContent = "Capítulos";
         backgroundTextElement.textContent = "Capítulos";
+
+        description = "Description for Cap - 2";
     }
+
+    currentTooltip = changeToolTip(
+      srcImages,
+      textElement.textContent,
+      description
+    );
   });
 
   images[i].addEventListener("mouseout", function () {
+    if (currentTooltip) {
+      currentTooltip.remove(); // Remove the tooltip element
+      currentTooltip = null; // Reset the currentTooltip variable
+    }
     textElement.textContent = "Capítulos";
     backgroundTextElement.textContent = "Capítulos";
   });
 }
 
-// okay, here we goooooooooo!!!!
+function changeToolTip(imageSrc, title, description) {
+  const tooltip = document.createElement("div");
+  tooltip.classList.add("tooltip");
 
-function changeToolTip() {
-  // yeeeahhh
+  const image = document.createElement("img");
+  image.src = imageSrc;
+  image.style.height = "270px"; // Set the image height
+  tooltip.appendChild(image);
+
+  const textContainer = document.createElement("div"); // Create a container div for the text elements
+  textContainer.classList.add("tooltipInformation");
+  tooltip.appendChild(textContainer);
+
+  const titleElement = document.createElement("p");
+  titleElement.textContent = title;
+  textContainer.appendChild(titleElement);
+
+  const descElement = document.createElement("p");
+  descElement.textContent = description;
+  descElement.style.wordWrap = "break-word"; // Set word-wrap property to break long words
+  descElement.style.maxWidth = "300px"; // Specify a maximum width for the element
+
+  textContainer.appendChild(descElement);
+
+  document.body.appendChild(tooltip);
+
+  document.addEventListener("mousemove", function (event) {
+    const tooltipWidth = tooltip.offsetWidth;
+    const tooltipHeight = tooltip.offsetHeight;
+
+    // Calculate the position based on the cursor's coordinates
+    let tooltipX = event.clientX + 150; // Add 10px offset to the right of the cursor
+
+    // Check if the tooltip exceeds the right edge of the screen
+    if (tooltipX + tooltipWidth > window.innerWidth - 200) {
+      tooltipX = event.clientX - tooltipWidth - 100; // Position it to the left of the cursor
+    }
+
+    // Set the tooltip position
+    tooltip.style.left = tooltipX + "px";
+    tooltip.style.top = event.clientY / 5 + tooltipHeight * 4.3 + "px";
+  });
+
+  return tooltip;
 }
